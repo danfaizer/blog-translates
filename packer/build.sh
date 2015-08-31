81a0b2f5aeb55aa77cbe3c2b6cf89a03a038499d
Daniel Beneyto <daniel.beneyto@schibsted.com>
2015-08-31 15:32:22 +0200

    refined travis.yml

--- packer/build.sh
+++ packer/build.sh
@@ -7,8 +7,8 @@ set -e
 # if [ -z "${AWS_SECRET_KEY+xxx}" ]; then echo AWS_SECRET_KEY is not set at all; exit 1;fi 
 # if [ -z "$AWS_SECRET_KEY" -a "${AWS_SECRET_KEY+xxx}" = "xxx" ]; then echo AWS_SECRET_KEY is set but empty;exit 1; fi
 
-tar --exclude "./.git*" --exclude "./packer" --exclude "./Gemfile.lock" --exclude "./translates.tar.gz" -zcf translates.tar.gz .
-mv translates.tar.gz packer/files/opt/translates.tar.gz
+#tar --exclude "./.git*" --exclude "./packer" --exclude "./Gemfile.lock" --exclude "./translates.tar.gz" -zcf translates.tar.gz .
+#mv translates.tar.gz packer/files/opt/translates.tar.gz
 
 export creator=$(git --no-pager show -s --format='%ae' ${TRAVIS_COMMIT})
 export creation_time=`date +"%Y%m%d%H%M%S"`
